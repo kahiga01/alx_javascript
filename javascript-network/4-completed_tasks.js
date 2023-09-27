8dfdad1e32b52242a2f69ebe8723c89396e66c02
Kahiga01 <danielkahiga69@gmail.com>
2023-09-27 10:12:36 -0700

    4. How many completed?

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,28 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
-
-const apiUrl = process.argv[2];
-
-request.get(apiUrl, { encoding: 'UTF-8' }, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  const todos = JSON.parse(body);
-  const completedTasksByUser = {};
-
-  todos.forEach((todo) => {
-    if (todo.completed) {
-      if (completedTasksByUser[todo.userId]) {
-        completedTasksByUser[todo.userId]++;
-      } else {
-        completedTasksByUser[todo.userId] = 1;
-      }
+request(process.argv[2], function (error, response, body) {
+    if (!error) {
+        const todos = JSON.parse(body);
+        const completed = {};
+        todos.forEach((todo) => {
+            if (todo.completed && completed[todo.userId] === undefined) {
+                completed[todo.userId] = 1;
+            } else if (todo.completed) {
+                completed[todo.userId] += 1;
+            }
+        });
+        console.log(completed);
     }
-  });
-
-  const jsonResult = JSON.stringify(completedTasksByUser, null, 2);
-  const formattedJsonResult = jsonResult.replace(/"/g, "'");
-  console.log(formattedJsonResult);
 });
